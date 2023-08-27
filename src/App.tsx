1b2ec5dbfc51110cdc1a87cd307a03e50aed5d04
Denys Horkavyi <denisdenis2295@gmail.com>
2023-08-27 20:20:40 +0200

    task: show newCard component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AppLayout } from './layouts/AppLayout/AppLayout'
 import './App.css'
 import NewCard from './layouts/NewCard/NewCard'
 import Card from './layouts/Card/Card'
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import CardListLayout from './layouts/CardList/CardListLayout'
 
 function App(): ReactElement {
@@ -13,28 +13,34 @@ function App(): ReactElement {
       question: 'What is the syntax for declaring a variable in JavaScript?',
       answer: 'Content for Card 1',
     },
-    // {
-    //   id: 2,
-    //   question: 'How do you write a comment in Javascript?',
-    //   answer: 'Content for Card 2',
-    // },
-    // {
-    //   id: 3,
-    //   question:
-    //     'What is difference between == and === operators in javascript?',
-    //   answer: 'Content for Card 3',
-    // },
-    // {
-    //   id: 4,
-    //   question: 'What is an array in JavaScript?',
-    //   answer: 'Content for Card 4',
-    // },
+    {
+      id: 2,
+      question: 'How do you write a comment in Javascript?',
+      answer: 'Content for Card 2',
+    },
+    {
+      id: 3,
+      question:
+        'What is difference between == and === operators in javascript?',
+      answer: 'Content for Card 3',
+    },
+    {
+      id: 4,
+      question: 'What is an array in JavaScript?',
+      answer: 'Content for Card 4',
+    },
   ]
+  const [cardsList, setCardsList] = useState(cards)
+  const [isAdded, setIsAdded] = useState<boolean>(false)
+  const handleAddCard = () => {
+    setIsAdded((prev) => !prev)
+  }
+
   return (
     <AppLayout>
-      <AppHeader />
-      <CardListLayout cards={cards} />
-      {/* <NewCard /> */}
+      <AppHeader cardsQty={cards.length} onClick={handleAddCard} />
+      {/* <CardListLayout cards={cards} /> */}
+      {isAdded && <NewCard />}
     </AppLayout>
   )
 }
